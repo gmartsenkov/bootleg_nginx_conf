02bc1f94dbda348f30501f852ab729ac025a4c48
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-02-10 11:42:50 +0000

    prepare for hex publishing

--- mix.exs
+++ mix.exs
@@ -6,8 +6,10 @@ defmodule BootlegNginxConf.MixProject do
       app: :bootleg_nginx_conf,
       version: "0.1.0",
       elixir: "~> 1.11",
+      description: "Bootleg task to upload nginx config to remote servers",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
@@ -21,9 +23,19 @@ defmodule BootlegNginxConf.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:bootleg, ">= 0.13.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:bootleg, ">= 0.13.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+    ]
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "bootleg_nginx_conf",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/gmartsenkov/bootleg_nginx_conf"}
     ]
   end
 end
